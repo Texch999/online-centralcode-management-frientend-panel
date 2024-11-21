b06d986d7ab7a5e57cf18061c2925bfbdcc1e917
sangram96rath <skesharirat96@gmail.com>
2024-11-21 19:11:21 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import SubHeader from "./components/SubHeader";
+import Homepage from "./pages/home/Homepage";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <SubHeader />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SubHeader() {
+  return (
+    <div>
+      <h1>SubHeader</h1>
+    </div>
+  );
+}
+
+export default SubHeader;
