e7fd7f0f2ca593aa5b47d108d54ec8e6da90df3a
sangram96rath <skesharirat96@gmail.com>
2024-11-22 12:44:38 +0530

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,17 +3,34 @@ import { Images } from "../images";
 
 function Login() {
   return (
-    <div className="login-bg w-100 h-100vh p-5">
-      <div className="white-bg w-100 h-fill login-box-shadow rounded-4 flex-between ">
-        <div className="w-50 pt-3 h-fill position-relative">
-          <div className="ps-4 pe-5 flex-column">
+    <div className="login-bg w-100 h-100vh p-5 d-flex justify-content-center align-items-center w-100">
+      <div
+        className="white-bg w-100 h-fill login-box-shadow rounded-4 flex-between"
+        style={{ maxWidth: "1000px" }}
+      >
+        <div className="w-50 pt-3 h-fill position-relative d-flex justify-content-center">
+          <div className="ps-4 pe-5 flex-column px-5 w-75">
             <div className="welcome-font">WELCOME</div>
             <div className="black-text">
               We are Glad to see you back with us
             </div>
             <div className="py-4">
-              <input className="input-css mt-2" placeholder="Username" />
-              <input className="input-css mt-2" placeholder="Password" />
+              <div className="d-flex align-items-center input-bg loginbox-radius my-2 px-2">
+                <img
+                  className="icon-img"
+                  alt="iconimg"
+                  src={Images.loginUserImages}
+                ></img>
+                <input className="input-css" placeholder="Username" />
+              </div>
+              <div className="d-flex align-items-center input-bg loginbox-radius my-2 px-2">
+                <img
+                  className="icon-img"
+                  alt="iconimg"
+                  src={Images.loginUserLock}
+                ></img>
+                <input className="input-css" placeholder="Password" />
+              </div>
               <button className="orange-btn mt-3 w-100">Submit</button>
             </div>
           </div>
--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,6 @@
 export const Images = {
   LoginImageOne: "assets/login_image_one.png",
   LoginImageTwo: "assets/login_image_two.png",
+  loginUserImages: "assets/loginPassword.png",
+  loginUserLock: "assets/loginUser.png",
 };
