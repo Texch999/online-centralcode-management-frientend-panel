0ad81c6d6e809cd12f4a438435564e81e66e7ac2
harishs7 <harish@sseventech.com>
2025-02-03 15:20:05 +0530

    endpoints are added those are missing in endpoints

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,206 +1,235 @@
 
-const userID = localStorage.getItem("user_id");
+const userID = () => localStorage.getItem("user_id");
 const endpoints = {
   // loginUser: { method: "post", url: "/master/login" },
-  createWebsite: { method: "post", url: `/user/${userID}/website/website` },
+  createWebsite: { method: "post", url: `/user/${userID()}/website/website` },
   updateWebsite: {
     method: "put",
-    url: (id) => `/user/${userID}/website/update/websiteby/${id}`,
+    url: (id) => `/user/${userID()}/website/update/websiteby/${id}`,
   },
   getWebsiteDetails: {
     method: "get",
-    url: (id) => `/user/${userID}/website/websiteby/${id}`,
+    url: (id) => `/user/${userID()}/website/websiteby/${id}`,
   },
   getWebsitesList: {
     method: "get",
     url: (params) => {
       const query = new URLSearchParams(params).toString();
-      return `/user/${userID}/website/websites?${query}`;
+      return `/user/${userID()}/website/websites?${query}`;
     },
   },
-  getAllCountires: { method: "get", url: `/user/${userID}/countries` },
+  getAllCountires: { method: "get", url: `/user/${userID()}/countries` },
   blockAndUnblock: {
     method: "put",
-    url: (id) => `/user/${userID}/website/block-unblock/${id}`,
+    url: (id) => `/user/${userID()}/website/block-unblock/${id}`,
   },
   getPromotionsTypes: {
     method: "get",
-    url: `/user/${userID}/api/getPromotionsTypes`,
+    url: `/user/${userID()}/api/getPromotionsTypes`,
   },
   getPromotionsImage: {
     method: "get",
-    url: `/user/${userID}/api/getPromotionsImages`,
+    url: `/user/${userID()}/api/getPromotionsImages`,
   },
   createPromotionImages: {
     method: "post",
-    url: `/user/${userID}/api/createPromotionImages`,
+    url: `/user/${userID()}/api/createPromotionImages`,
   },
   statusPromotionsTypes: {
     method: "put",
-    url: (id) => `/user/${userID}/api/statusPromotionsTypes/${id}`,
+    url: (id) => `/user/${userID()}/api/statusPromotionsTypes/${id}`,
   },
   deletePromotionsImages: {
     method: "delete",
-    url: (id) => `/user/${userID}/api/statusPromotionsTypes/${id}`,
+    url: (id) => `/user/${userID()}/api/statusPromotionsTypes/${id}`,
   },
 
   loginUser: { method: "post", url: "/master/login" },
   // addManagemnentTeam: { method: "post", url: "/employee" },
-  addManagemnentTeam: { method: "post", url: `/user/${userID}/employee` },
+  addManagemnentTeam: { method: "post", url: `/user/${userID()}/employee` },
 
-  getRoles: { method: "get", url: `/user/${userID}/rolesList` },
-  // getEmployees: { method: "get", url: `/user/${userID}/employees` },
+  getRoles: { method: "get", url: `/user/${userID()}/rolesList` },
+  // getEmployees: { method: "get", url: `/user/${userID()}/employees` },
   // getEmployees: ({ limit, offset }) => ({
   //   method: "get",
-  //   url: `/user/${userID}/employeeeees?limit=${limit}&offset=${offset}`,
+  //   url: `/user/${userID()}/employeeeees?limit=${limit}&offset=${offset}`,
   // }),
 
   // getEmployees: {
   //   method: "get",
   //   url: (params) => {
   //     const query = new URLSearchParams(params).toString();
-  //     return `/user/${userID}/employees?${query}`;
+  //     return `/user/${userID()}/employees?${query}`;
   //   },
   // },
   getEmployees: {
     method: "get",
     url: (params) => {
       const query = new URLSearchParams(params).toString(); // Dynamically build the query with parameters
-      return `/user/${userID}/employees?${query}`;
+      return `/user/${userID()}/employees?${query}`;
     },
   },
   getDirectors: {
     method: "get",
     url: (params) => {
       const query = new URLSearchParams(params).toString();
-      return `/user/${userID}/directors?${query}`;
+      return `/user/${userID()}/directors?${query}`;
     },
   },
   resetEmployeePassword: {
     method: "post",
-    url: (id) => `/user/${userID}/employeeUpdatePassword/${id}`,
+    url: (id) => `/user/${userID()}/employeeUpdatePassword/${id}`,
   },
   resetDirectorPassword: {
     method: "post",
-    url: (id) => `/user/${userID}/directorUpdatePassword/${id}`,
+    url: (id) => `/user/${userID()}/directorUpdatePassword/${id}`,
   },
   blockEmploye: {
     method: "post",
-    url: (id) => `/user/${userID}/employeeBlockUnblock/${id}`,
+    url: (id) => `/user/${userID()}/employeeBlockUnblock/${id}`,
   },
   blockDirector: {
     method: "post",
-    url: (id) => `/user/${userID}/directorBlockUnblock/${id}`,
+    url: (id) => `/user/${userID()}/directorBlockUnblock/${id}`,
   },
 
   updateEmployeeByID: {
     method: "post",
-    url: (id) => `/user/${userID}/employee/${id}`,
+    url: (id) => `/user/${userID()}/employee/${id}`,
   },
   getEmployeeDetailsById: {
     method: "get",
-    url: (id) => `/user/${userID}/employee/${id}`,
+    url: (id) => `/user/${userID()}/employee/${id}`,
   },
 
   //security questions
   createSecurityQuestions: {
     method: "post",
-    url: `/user/${userID}/secQuestion`,
+    url: `/user/${userID()}/secQuestion`,
   },
   getAllSecurityQuestions: {
     method: "get",
-    url: `/user/${userID}/secQuestion/`,
+    url: `/user/${userID()}/secQuestion/`,
   },
   getSecQusetionsById: {
     method: "get",
-    url: (id) => `/user/${userID}/secQuestion/${id}`,
+    url: (id) => `/user/${userID()}/secQuestion/${id}`,
   },
   updateSecurityQuestions: {
     method: "put",
-    url: (id) => `/user/${userID}/secQuestion/${id}`,
+    url: (id) => `/user/${userID()}/secQuestion/${id}`,
   },
   //rejection reasons
   getAllRejectionReasons: {
     method: "get",
-    url: `/user/${userID}/rejectionReasons/`,
+    url: `/user/${userID()}/rejectionReasons/`,
   },
   createRejReasons: {
     method: "post",
-    url: `/user/${userID}/rejectionReasons/`,
+    url: `/user/${userID()}/rejectionReasons/`,
   },
   updateRejReasons: {
     method: "put",
-    url: (id) => `/user/${userID}/rejectionReasons/${id}`,
+    url: (id) => `/user/${userID()}/rejectionReasons/${id}`,
   },
   getRejReasonsById: {
     method: "get",
-    url: (id) => `/user/${userID}/rejectionReasons/${id}`,
+    url: (id) => `/user/${userID()}/rejectionReasons/${id}`,
   },
   // privacy policy
   getPrivacyPolicy: {
     method: "get",
-    url: `/user/${userID}/privacypolicies/`,
+    url: `/user/${userID()}/privacypolicies/`,
   },
   getPrivacyPolicyById: {
     method: "get",
-    url: (id) => `/user/${userID}/privacypolicies/${id}`,
+    url: (id) => `/user/${userID()}/privacypolicies/${id}`,
   },
   createPrivacyPolicy: {
     method: "post",
-    url: `/user/${userID}/privacypolicies/`,
+    url: `/user/${userID()}/privacypolicies/`,
   },
   updatePrivacyPolicyById: {
     method: "put",
-    url: (id) => `/user/${userID}/privacypolicies/${id}`,
+    url: (id) => `/user/${userID()}/privacypolicies/${id}`,
   },
   privacyPolicyStatusUpdate: {
     method: "patch",
     url: (data) =>
-      `/user/${userID}/privacypolicies/${data.id}/status/${data.status}`,
+      `/user/${userID()}/privacypolicies/${data.id}/status/${data.status}`,
   },
 
   getCountries: {
     method: "get",
-    url: `/user/${userID}/countries/countries`,
+    url: `/user/${userID()}/countries/countries`,
   },
-  getWebsites: { method: "get", url: `/user/${userID}/website/websites` },
+  getWebsites: { method: "get", url: `/user/${userID()}/website/websites` },
   getDirectorEmployeesLoginLogsList: {
     method: "get",
     url: (params) => {
       const query = new URLSearchParams(params).toString();
-      return `/user/${userID}/directorEmployeeloginLogsbyDirector?${query}`;
+      return `/user/${userID()}/directorEmployeeloginLogsbyDirector?${query}`;
     },
   },
   getDirectorEmployeesLoginLogsByEmployeeId: {
     method: "get",
     url: (params) => {
       const query = new URLSearchParams(params).toString();
-      return `/user/${userID}/DirectorEmploginLogsbyEmployeeId?${query}`;
+      return `/user/${userID()}/DirectorEmploginLogsbyEmployeeId?${query}`;
     },
   },
-  // getWebsites: { method: "get", url: `user/${userID}/website/websites` },
+  // getWebsites: { method: "get", url: `user/${userID()}/website/websites` },
   getAvailableWebsites: {
     method: "get",
-    url: (id) => `user/${userID}/privacypolicies/${id}/websites`,
+    url: (id) => `user/${userID()}/privacypolicies/${id}/websites`,
   },
-  createDirector: { method: "post", url: `/user/${userID}/create` },
+  createDirector: { method: "post", url: `/user/${userID()}/create` },
   createBroadCasting: {
     method: "post",
-    url: `/user/${userID}/api/broadcasting`,
+    url: `/user/${userID()}/api/broadcasting`,
   },
   getBroadCasting: {
     method: "get",
-    url: `/user/${userID}/api/broadcasting`,
+    url: `/user/${userID()}/api/broadcasting`,
   },
   statusBroadcastUpdate: {
     method: "put",
-    url: (id) => `/user/${userID}/api/broadcasting/statusBroadcastUpdate/${id}`,
+    url: (id) => `/user/${userID()}/api/broadcasting/statusBroadcastUpdate/${id}`,
   },
   editBroadCasting: {
     method: "put",
-    url: (id) => `/user/${userID}/api/statusPromotionsTypes/${id}`,
+    url: (id) => `/user/${userID()}/api/statusPromotionsTypes/${id}`,
   },
   loginDirector: { method: "post", url: "/director/login" },
+
+  getLoggedInLogs: {
+    method: "get",
+    url: (params) => {
+      const query = new URLSearchParams(params).toString();
+      return `/user/${userID()}/loginLogs?${query}`;
+    },
+  },
+  getLoggedInLogsById: {
+    method: "get",
+    url: (params) => {
+      const query = new URLSearchParams(params).toString();
+      return `/user/${userID()}/loginLogsById?${query}`;
+    },
+  },
+  getDirectorLoginLogs: {
+    method: "get",
+    url: (params) => {
+      const query = new URLSearchParams(params).toString();
+      return `/user/${userID()}/getParentLoginLogs?${query}`;
+    },
+  },
+  getDirectorLoginLogsById: {
+    method: "get",
+    url: (params) => {
+      const query = new URLSearchParams(params).toString();
+      return `/user/${userID()}/getParentLoginLogsById?${query}`;
+    },
+  }
 }
 
 export default endpoints;
