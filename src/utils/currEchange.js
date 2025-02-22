6af1c7682f8e5932879c0f531ef41af8c696f888
harishs7 <harish@sseventech.com>
2025-02-22 11:24:27 +0530

    chnages

--- src/utils/currEchange.js
+++ src/utils/currEchange.js
@@ -2,22 +2,22 @@
 //a mount default 1
 // first curr value 
 const currencyConvert = (amount, firstCur, secCur) => {
-    
-    const secFin = (parseFloat(amount)/parseFloat(firstCur)) * parseFloat(secCur);
+
+    const secFin = (parseFloat(amount) / parseFloat(firstCur)) * parseFloat(secCur);
     return secFin;
-   }
-   
-   // Convert Any Currency To INR
-   const convertChipsToInr = (chipVal, amount, exchange) => {
+}
+
+// Convert Any Currency To INR
+const convertChipsToInr = (chipVal, amount, exchange) => {
     const amountInr = parseFloat(amount) / parseFloat(exchange);
     const inrChips = parseFloat(amountInr) * parseFloat(chipVal);
     return inrChips;
-   }
-   
-   // TODO Convert Chips To Currency 
-   const convChipsToCur = (chipVal, totalChops, exchange) => {
+}
+
+// TODO Convert Chips To Currency 
+const convChipsToCur = (chipVal, totalChops, exchange) => {
     const inrChips = (parseFloat(totalChops) / parseFloat(chipVal)) * parseFloat(exchange);
     return inrChips;
-   }
-   
-   module.exports = {currencyConvert, convertChipsToInr, convChipsToCur};
+}
+
+module.exports = { currencyConvert, convertChipsToInr, convChipsToCur };
