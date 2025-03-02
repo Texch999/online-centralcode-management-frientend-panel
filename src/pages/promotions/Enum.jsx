a2dd332973e7da706decbe70fb94e5cf9334d8f3
srilakshmireact <srilakshmi.react11@gmail.com>
2025-03-02 12:31:32 +0530

    Enums

---
+++ src/pages/promotions/Enum.jsx
@@ -0,0 +1,39 @@
+const Enums = {
+  selectOptionsType: {
+    Sports: 1,
+    Casino: 2,
+  },
+
+  diamondSelectPages: {
+    "HOME": 1,
+    "IN PLAY": 2,
+    "ELECTIONS": 3,
+    "CRICKET": 4,
+    "FOOT BALL": 5,
+    "TENNIS": 6,
+    "GREY HOUND": 7,
+    "HOURSE RACING": 8,
+    "CASINO": 9,
+    "MULTI MARKET": 10,
+    "SOCCER": 11,
+    "BASCKET BALL": 12,
+    "KABADDI": 13,
+    "ICE HOCKEY": 14,
+    "VOLLEY BALL": 15,
+    "TABLE TENNIS": 16,
+    // "KABADDI": 9,
+    // "KABADDI": 9,
+  },
+  diamondSelectPlace: {
+    Top: 1,
+  },
+  //   selectPlace: {
+  //     Top: 1,
+  //     Center: 2,
+  //     Bottonm: 3,
+  //     Right: 4,
+  //     Left: 5,
+  //   },
+};
+
+export default Enums;
