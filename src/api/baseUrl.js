a92766a17c0e702f5b14f03a7dce9fc17c17f44d
Ganga <gangarathnam@sseventech.com>
2025-03-07 17:07:58 +0530

    url

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,6 @@
 // export const baseUrl = "http://localhost:9001/rest2/0.1";
-export const baseUrl = "http://192.168.29.187:9001/rest2/0.1";
-export const imgUrl = "http://192.168.29.187:9001/uploads";
+export const baseUrl = "http://192.168.29.82:9001/rest2/0.1";
+export const imgUrl = "http://192.168.29.82:9001/uploads";
 // export const baseUrl = "https://online.backend.7boundaries.com/rest2/0.1";
 // export const imgUrl = "https://online.backend.7boundaries.com/uploads";
 
