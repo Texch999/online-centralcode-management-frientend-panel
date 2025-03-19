958d93055f8cf85668a363f46353ec4b266970ea
Ganga <gangarathnam@sseventech.com>
2025-03-19 16:28:26 +0530

    chnages

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,10 +1,8 @@
-export const baseUrl = "http://localhost:9001/rest2/0.1";
 // export const baseUrl = "http://localhost:9001/rest2/0.1";
-export const imgUrl = "http://localhost:9001/uploads";
+// export const baseUrl = "http://localhost:9001/rest2/0.1";
+// export const imgUrl = "http://localhost:9001/uploads";
 // export const baseUrl = "https://online.backend.7boundaries.com/rest2/0.1";
 // export const imgUrl = "https://online.backend.7boundaries.com/uploads";
 
-// export const baseUrl = "http://192.168.29.186:9001/rest2/0.1";
-// export const baseUrl="https://2f55-2405-201-c030-60bb-515c-377e-4a40-b87d.ngrok-free.app/rest2/0.1"
-// export const imgUrl = "http://192.168.29.186:9001/uploads";
-// export const baseUrl = "http://192.168.29.186:9001/rest2/0.1";
+export const baseUrl = "http://192.168.29.186:9001/rest2/0.1";
+export const imgUrl = "https://gameappxyz.s3.ap-south-1.amazonaws.com";
