4d1f5f70e9e5df3025009460e368efa0471a8530
Pranay <pranay@sseventech.com>
2025-03-19 23:29:32 +0530

    pulled

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,8 +1,8 @@
-export const baseUrl = "http://localhost:9001/rest2/0.1";
 // export const baseUrl = "http://localhost:9001/rest2/0.1";
-export const imgUrl = "http://localhost:9001/uploads";
+// export const baseUrl = "http://localhost:9001/rest2/0.1";
+// export const imgUrl = "http://localhost:9001/uploads";
 // export const baseUrl = "https://online.backend.7boundaries.com/rest2/0.1";
 // export const imgUrl = "https://online.backend.7boundaries.com/uploads";
 
-// export const baseUrl = "https://1cee-2405-201-c030-60bb-7280-c176-49d9-2ede.ngrok-free.app/rest2/0.1";
-// export const imgUrl = "https://gameappxyz.s3.ap-south-1.amazonaws.com";
+export const baseUrl = "http://192.168.29.186:9001/rest2/0.1";
+export const imgUrl = "https://gameappxyz.s3.ap-south-1.amazonaws.com";
