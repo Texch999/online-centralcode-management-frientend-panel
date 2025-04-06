d4153873e82a2542410040ab7a83b2d8fa1815b8
Pranay <pranay@sseventech.com>
2025-04-06 15:11:43 +0530

    pulled

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://192.168.29.186:9001/rest2/0.1";
+export const baseUrl = "https://online.backend.7boundaries.com/rest2/0.1";
 export const imgUrl = "https://gameappxyz.s3.ap-south-1.amazonaws.com";
