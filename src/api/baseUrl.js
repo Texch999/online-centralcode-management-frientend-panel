f549bb3c1360969717285c1c3042153dba7e35c7
Ganga <gangarathnam@sseventech.com>
2025-05-10 19:51:32 +0530

    chnages

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl =
-"https://f0d4-2405-201-c030-60c8-2ea1-de0f-653a-341.ngrok-free.app/rest2/0.1";
-//  export const baseUrl = "http://192.168.29.218:9001/rest2/0.1";
+// export const baseUrl =
+// "https://f0d4-2405-201-c030-60c8-2ea1-de0f-653a-341.ngrok-free.app/rest2/0.1";
+export const baseUrl = "http://192.168.29.218:9001/rest2/0.1";
 export const imgUrl = "https://gameappxyz.s3.ap-south-1.amazonaws.com";
